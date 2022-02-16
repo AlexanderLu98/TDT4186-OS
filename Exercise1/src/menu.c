f23c2c7c334a4ec9d854d8f0d0004c297ed9894b
alfhusem <alfbhusem@gmail.com>
2022-02-16 16:36:38 +0100

    Added comments to menu.c

--- Exercise1/src/menu.c
+++ Exercise1/src/menu.c
@@ -4,21 +4,24 @@
 
 int main() {
 
+    // Variable that holds the users choice as a char
     char choice;
 
+    // Get the current time
     time_t mytime = time(NULL);
     char * time_str = ctime(&mytime);
     time_str[strlen(time_str)-1] = '\0';
 
     do {
-        //system("cls") //what
-        //printf("\n");
+        // Print Welcome screen with menu options
         printf("\nWelcome to the alarm clock! It is currently ");
         printf("%s",time_str);
         printf("\nPlease enter \"s\" (schedule), \"l\" (list), \"c\" (cancel), \"x\" (exit)\n");
 
+        // Read user input
         scanf("%c", &choice);
 
+        // Determine what method should be called based on input
         switch (choice) {
             case 's':
                 printf("S"); //schedule();

920fa412054dc05647d44691fc40d50836c5d62d
Hallvardd <Hallvste@stud.ntnu.no>
2022-04-13 18:52:18 +0200

    reads path now

--- Exercise3/src/flush.c
+++ Exercise3/src/flush.c
@@ -6,8 +6,10 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <limits.h>
+#include <errno.h>
 #define MAX_PATH_LENGTH 2048
 #define MAX_CMD_LENGTH 4096
+
 
 // Helper method to flush stdin.
 void flush_in(void)
@@ -16,31 +18,51 @@ void flush_in(void)
   while ((ch = getchar()) != '\n' && ch != EOF);
 }
 
+void change_directory(char* buffer)
+{
+  int error = 0; 
+  strtok(buffer, " "); // removing first token
+  error = chdir(strtok(NULL, " "));
+  if(error != 0)
+  {    
+    perror("Error");
+  } 
+}
+
 int main()
 {
-  char cwd[MAX_PATH_LENGTH]; //current working directory
-  char cmd[MAX_CMD_LENGTH];  //the command
-  char bfr[MAX_CMD_LENGTH];  //buffer for parsing the commnand
-  char tkn[MAX_CMD_LENGTH];  //buffer for storing each token
+  char* PATH = getenv("PATH"); // getting the path variable
+  char  cwd[MAX_PATH_LENGTH]; //current working directory
+  char  cmd[MAX_CMD_LENGTH];  //the command
+  char  bfr[MAX_CMD_LENGTH];  //buffer for parsing the commnand
+  char* tkn;  //buffer for storing each token
+  char** tokens;
   int exit = 0;
 
-  getcwd(cwd, sizeof(cwd)); // getting the working directory
-
+  printf("%s/n", PATH);
   while(!exit)
   {
+    getcwd(cwd, sizeof(cwd)); // getting the working directory
     printf("%s: ", cwd);
-    fgets(bfr, sizeof(cmd), stdin);
-    strcpy(cmd, strtok(bfr, " "));
-    if(strcmp(cmd, "cd\n") == 0)
+    fgets(cmd, sizeof(cmd), stdin);
+    cmd[strcspn(cmd, "\r\n\0")] = 0; // removing the /n charracter
+    strcpy(bfr, cmd);
+    tkn = strtok(bfr, " ");
+    if(strcmp(tkn, "cd") == 0)
     {
-      printf("got command %s", cmd);
+      printf("got command %s \n", cmd);
+      //tkn = strtok(NULL, " ");
+      //printf("%s\n", tkn);
+      //chdir(tkn);
+      //perror("Got this error:");
+      change_directory(cmd);
     }
-    else if(strcmp(cmd, "ls\n") == 0)
+    else if(strcmp(cmd, "ls") == 0)
     {
-      printf("got command %s", cmd);
+      printf("got command %s \n", cmd);
     }
 
-    else if(strcmp(cmd, "exit\n") == 0)
+    else if(strcmp(cmd, "exit") == 0)
     {
       break;
     }
